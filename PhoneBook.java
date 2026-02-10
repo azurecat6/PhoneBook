3a3cdc50cff8df6f966ec0518459a31e59d96269
Azure cat <6azurecat6@gmail.com>
2026-02-10 17:43:17 +0300

    PhoneBook.java

--- PhoneBook.java
+++ PhoneBook.java
@@ -3,7 +3,7 @@ package lessons;
 import java.util.*;
 import java.util.stream.Collectors;
 
-public class PhoneBook {
+public class PhoneBook implements Runnable {
 
     private List<Contact> contacts = new ArrayList<>();
 
